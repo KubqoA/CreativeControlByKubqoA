6b2ce753ec05870ce162c6a10a201fb466b5c1ab
Jakub Arbet <arbetjakub@gmail.com>
2018-08-08 21:31:07 +0200

    Initial commit
    Signed-off-by: Jakub Arbet <arbetjakub@gmail.com>

---
+++ src/main/java/io/github/kubqoa/creativecontrolbykubqoa/CreativeControlByKubqoA.java
@@ -0,0 +1,18 @@
+package io.github.kubqoa.creativecontrolbykubqoa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CreativeControlByKubqoA extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
